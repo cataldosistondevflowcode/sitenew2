d981c95d34c215e96d76a1dfe48785a3e8946736
lucasaugustodev <lucas@algoricca.com>
2025-06-23 18:00:13 -0300

    ajuste

--- src/components/SocialBar.tsx
+++ src/components/SocialBar.tsx
@@ -5,7 +5,8 @@ import {
   FacebookIcon, 
   InstagramIcon, 
   JusticeIcon, 
-  YouTubeIcon 
+  YouTubeIcon,
+  WhatsAppIcon
 } from './icons';
 
 export const ContactInfo: React.FC = () => {
@@ -23,13 +24,13 @@ export const ContactInfo: React.FC = () => {
         </a>
       </address>
 
-      <address className="absolute h-[26px] left-[336px] top-[3px] w-[177px] max-md:relative max-md:top-auto max-md:left-auto max-md:w-auto max-md:h-auto max-sm:w-full max-sm:text-center">
-        <div className="flex absolute top-0 left-0 shrink-0 items-center py-0.5 pr-40 pl-0 h-[26px] w-[177px] max-sm:p-0 max-sm:w-full">
+      <address className="absolute h-[26px] left-[336px] top-[3px] w-[200px] max-md:relative max-md:top-auto max-md:left-auto max-md:w-auto max-md:h-auto max-sm:w-full max-sm:text-center">
+        <div className="flex absolute top-0 left-0 shrink-0 items-center py-0.5 pr-40 pl-0 h-[26px] w-[200px] max-sm:p-0 max-sm:w-full">
           <PhoneIcon />
         </div>
         <a
           href="tel:+552131733795"
-          className="absolute top-0.5 shrink-0 text-lg leading-7 text-white h-[22px] left-[30px] w-[147px] max-sm:left-0 max-sm:w-full max-sm:text-sm max-sm:top-[30px] hover:underline"
+          className="absolute top-0.5 shrink-0 text-lg leading-7 text-white h-[22px] left-[30px] w-[170px] max-sm:left-0 max-sm:w-full max-sm:text-sm max-sm:top-[30px] hover:underline"
         >
           +55 (21) 3173-3795
         </a>
@@ -41,9 +42,14 @@ export const ContactInfo: React.FC = () => {
 export const ContactButton: React.FC = () => {
   return (
     <a
-      href="#contact"
-      className="flex absolute top-0 gap-1 justify-center items-start pt-1.5 pr-2 pb-1 pl-1 rounded-md h-[31px] left-[517px] w-[139px] max-md:relative max-md:top-auto max-md:left-auto max-md:w-auto max-sm:p-2.5 max-sm:w-full max-sm:text-center hover:bg-neutral-600 transition-colors"
+      href="https://wa.me/552131733795"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex absolute top-0 gap-1 justify-center items-start pt-1.5 pr-2 pb-1 pl-1 rounded-md h-[31px] left-[540px] w-[139px] max-md:relative max-md:top-auto max-md:left-auto max-md:w-auto max-sm:p-2.5 max-sm:w-full max-sm:text-center hover:bg-neutral-600 transition-colors"
     >
+      <div className="absolute left-1 top-1.5 h-[21px] w-[21px]">
+        <WhatsAppIcon />
+      </div>
       <span className="absolute h-5 text-base font-bold leading-6 text-white left-[29px] top-[7px] w-[103px] max-sm:left-2/4 max-sm:text-sm max-sm:-translate-x-2/4">
         Fale Conosco
       </span>
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -3,4 +3,5 @@ export { PhoneIcon } from './PhoneIcon';
 export { FacebookIcon } from './FacebookIcon';
 export { InstagramIcon } from './InstagramIcon';
 export { JusticeIcon } from './JusticeIcon';
-export { YouTubeIcon } from './YouTubeIcon'; 
+export { YouTubeIcon } from './YouTubeIcon';
+export { WhatsAppIcon } from './WhatsAppIcon'; 
